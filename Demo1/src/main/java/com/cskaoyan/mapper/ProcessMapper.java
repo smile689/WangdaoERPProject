f585447a0a37115f5a1508e0de50fb99097530be
ConanRookie <1147037965.@qq.com>
2018-12-06 09:56:58 +0800

    update for generator

---
+++ Demo1/src/main/java/com/cskaoyan/mapper/ProcessMapper.java
@@ -0,0 +1,17 @@
+package com.cskaoyan.mapper;
+
+import com.cskaoyan.bean.Process;
+
+public interface ProcessMapper {
+    int deleteByPrimaryKey(String processId);
+
+    int insert(Process record);
+
+    int insertSelective(Process record);
+
+    Process selectByPrimaryKey(String processId);
+
+    int updateByPrimaryKeySelective(Process record);
+
+    int updateByPrimaryKey(Process record);
+}
