711684197c7aed2d1f43bdd9ecd7f231c9620382
GarenW <512191350@qq.com>
2018-12-06 09:48:18 +0800

    update for generator

--- Demo1/src/main/java/com/cskaoyan/mapper/DepartmentMapper.java
+++ Demo1/src/main/java/com/cskaoyan/mapper/DepartmentMapper.java
@@ -3,6 +3,14 @@ package com.cskaoyan.mapper;
 import com.cskaoyan.bean.Department;
 
 public interface DepartmentMapper {
+    int deleteByPrimaryKey(String departmentId);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    Department selectByPrimaryKey(String departmentId);
+
     int updateByPrimaryKeySelective(Department record);
 
     int updateByPrimaryKey(Department record);
