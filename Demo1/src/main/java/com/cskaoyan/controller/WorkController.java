e0046992fbe616b872f6fb6024049c43a0bed51c
smile689 <shiweixiao1990@163.com>
2018-12-08 09:22:29 +0800

    完成产品，客户，部分订单

--- Demo1/src/main/java/com/cskaoyan/controller/WorkController.java
+++ Demo1/src/main/java/com/cskaoyan/controller/WorkController.java
@@ -1,4 +1,17 @@
 package com.cskaoyan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/work")
 public class WorkController {
+
+
+    @RequestMapping("/find")
+    public String findWorkListJsp(){
+        return "work_list";
+    }
+
+
 }
